2e998e7637a4505699aa92f196231525144fd75c
Ghassen Laajailia <61318259+ghass002@users.noreply.github.com>
2020-06-12 23:55:59 +0200

    Add files via upload
    Load the Internet Movie Database (IMDb) movie reviews dataset. This dataset contains movie reviews posted by people on the IMDb website, as well as the corresponding labels (“positive” or “negative”) indicating whether the reviewer liked the movie or not.

---
+++ explore_data.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_num_words_per_sample(sample_texts):
+    """Returns the median number of words per sample given corpus.
+
+    # Arguments
+        sample_texts: list, sample texts.
+
+    # Returns
+        int, median number of words per sample.
+    """
+    num_words = [len(s.split()) for s in sample_texts]
+    return np.median(num_words)
+
+def plot_sample_length_distribution(sample_texts):
+    """Plots the sample length distribution.
+
+    # Arguments
+        samples_texts: list, sample texts.
+    """
+    plt.hist([len(s) for s in sample_texts], 50)
+    plt.xlabel('Length of a sample')
+    plt.ylabel('Number of samples')
+    plt.title('Sample length distribution')
+    plt.show()
